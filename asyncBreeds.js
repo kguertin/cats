3d62ed98c61fe00f9d1923d48eeb4fc5554a5fdf
Kevin Guertin <kevin.m.guertin@gmail.com>
2020-02-11 23:22:56 +0000

    added testing for async functions

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -5,7 +5,11 @@ const breedDetailsFromFile = function(breed, functionToRunWhenThingsAreDone) {
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
     // CHANGE: Pass data into callback instead of returning it directly
     console.log("In readFile's Callback: it has the data.");
-    if (!error) functionToRunWhenThingsAreDone(data);
+    if(!error) {
+      functionToRunWhenThingsAreDone(data);
+    } else {
+      functionToRunWhenThingsAreDone(undefined)
+    }
   });
 };
 
@@ -16,6 +20,8 @@ const printOutCatBreed = breed => {
 
 // CHANGE 2: we're now passing two arguments into breedDetailsFromFile: breed string and a callback function
 breedDetailsFromFile('Bombay', printOutCatBreed);
+
+module.exports = breedDetailsFromFile;
 
 
 
